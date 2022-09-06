5e2bbf11c0deb2d7410348041b16cf55619e5cfd
Serhii Petrash <serhiipetrash@gmail.com>
2022-09-06 21:45:32 +0300

    clear start project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<template >
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h2>Home</h2>
+  </div>
+
 </template>
